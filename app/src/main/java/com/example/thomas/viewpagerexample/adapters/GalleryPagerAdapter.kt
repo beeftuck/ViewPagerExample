98bf815e01b063600facb1dc7050b83e818bba46
Thomas-EET <thomas@eagleeyetechnology.com>
2018-07-02 15:53:13 +0100

    Code cleanup

--- app/src/main/java/com/example/thomas/viewpagerexample/adapters/GalleryPagerAdapter.kt
+++ app/src/main/java/com/example/thomas/viewpagerexample/adapters/GalleryPagerAdapter.kt
@@ -6,9 +6,7 @@ import android.support.v4.app.FragmentStatePagerAdapter
 import com.example.thomas.viewpagerexample.fragments.GalleryPageFragment
 
 class GalleryPagerAdapter(fm: FragmentManager?, private val data: List<String>) : FragmentStatePagerAdapter(fm) {
-
-
-
+    
     override fun getItem(position: Int): Fragment {
         return GalleryPageFragment.newInstance(data[position])
     }
